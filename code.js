2ffa1fb8984b4adfddb8abc496e4fcccb9c86d3b
Alex Goodvin <78442732+Goodvin707@users.noreply.github.com>
2021-02-02 23:14:46 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,11 @@
+    window.addEventListener('load', function () {
+
+        let butPage = document.getElementById("Button1");
+        let butStyle = document.getElementById("Button2");
+        butPage.addEventListener('click', function (){
+          window.location.href = "http://www.mathprofi.ru/kompleksnye_chisla_dlya_chainikov.html#k";
+        });
+        butStyle.addEventListener('click', function(){
+          document.body.style.backgroundColor = "green";
+        });
+    });
